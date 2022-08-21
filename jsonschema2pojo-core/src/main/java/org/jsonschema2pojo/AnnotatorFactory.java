745f7c327c062c8f5a0ed97af1047bafdbe31f65
Lincest <474617032@qq.com>
2022-08-21 18:53:32 +0800

    Revert "Add FastJson 1.x annotation style (for maven plugin only)."
    This reverts commit d75feffcec2c93ee2aa4352acc5d70c35dc02a67.

--- jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/AnnotatorFactory.java
+++ jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/AnnotatorFactory.java
@@ -54,8 +54,6 @@ public class AnnotatorFactory {
                 return new GsonAnnotator(generationConfig);
             case MOSHI1:
                 return new Moshi1Annotator(generationConfig);
-            case FASTJSON:
-                return new FastJson1Annotator(generationConfig);
             case NONE:
                 return new NoopAnnotator();
             default:
